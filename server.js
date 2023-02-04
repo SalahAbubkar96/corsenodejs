2f2dd8344cee77b46e96248f73311dcd3a2b443a
Salah Abubkar <salahabubkar96@gmail.com>
2023-02-04 14:03:12 +0400

    first commit

---
+++ server.js
@@ -0,0 +1,27 @@
+const http= require('http')
+
+
+
+const server= http.createServer((req,res)=>{
+
+    if(req.url=='/home'){
+        res.statusCode=200
+        res.write('welcome from home')
+    }else if(req.url=='/contact'){
+        res.statusCode=200
+        res.write('welcome from contact')
+    }else if(req.url=='/about'){
+        res.statusCode=200
+        res.write('welcme from about')
+    }else{
+        res.statusCode=404
+        res.write('not fond')
+    }
+ 
+
+
+
+ res.end()
+})
+
+server.listen(5000,()=>{console.log('server run with port 5000')})
